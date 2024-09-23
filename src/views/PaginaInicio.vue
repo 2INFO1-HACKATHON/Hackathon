95e6d72b3d8a4cedef7b67bec1b945bb45801afb
bokorni <lucasbokorni@gmail.com>
2024-09-23 11:27:08 -0300

    terminar

--- src/views/PaginaInicio.vue
+++ src/views/PaginaInicio.vue
@@ -2,10 +2,29 @@
 <template>
   <div class="container">
     <div class="header">
-      <RouterLink to="/"> <img src="../assets/imagens/logo.png" alt=""></RouterLink>
-     
+      <RouterLink to="/"> <img src="../assets/imagens/logo.png" alt="" /></RouterLink>
+      <button @click="scrollToSection" class="scroll-button">Sobre Nós</button>
+      <button>Serviços</button>
+      <RouterLink to="PaginaLogin">Entrar</RouterLink>
+      <RouterLink to="PaginaSignup">Criar Conta</RouterLink>
+    </div>
+    <div class="content">
+      
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  background-color: rgb(0, 0, 0);
+  height: 3vh;
+  display: flex;
+  justify-content: start;
+}
+img {
+  width: 160px;
+  height: 130px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
